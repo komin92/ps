d3e77196b0bdde442d3462f73b55238d30fe6073
Eduard Prinz <callby.reference@gmx.at>
2018-08-05 11:58:31 +0200

    Add files via upload

--- searchadmin.ps1
+++ searchadmin.ps1
@@ -1,19 +1,15 @@
 
 # Search AD for comptuer objects
-$ComputerInfoFile="c:\\test.txt"
-$ObjFilter = "(objectClass=user)" #update the filter with Server specfic filter based on your requirement. 
+$ObjFilter = "(objectClass=Computer)" #update the filter with Server specfic filter based on your requirement. 
 $objSearch = New-Object System.DirectoryServices.DirectorySearcher
 $objSearch.PageSize = 15000
 $objSearch.Filter = $ObjFilter
 $dsn = get-addomain | select -expand distinguishedname
-$objsearch.searchroot = "LDAP://$dsn"
+$objsearch.searchroot = "ldap://$dsn"
 $AllObj = $objSearch.FindAll()
 foreach ($Obj in $AllObj)
 	   	{	
 			$objItemT = $Obj.Properties
 			$CName = $objItemT.name
-			$objItemT
-			write-host "`n"
-			$Obj
-			write-host "`n"
+			"$CName" | Out-File $ComputerInfoFile -encoding ASCII -append
 		}
